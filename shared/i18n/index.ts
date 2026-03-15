c53175523c3b97bc1aa3f9617dbdfccaabfbfe01
wojzj57 <wojzj57@gmail.com>
2026-03-15 18:03:22 +0800

    chore(i18n): 注释掉部分语言支持

--- shared/i18n/index.ts
+++ shared/i18n/index.ts
@@ -12,90 +12,90 @@ export const languageOptions: LanguageOption[] = [
     label: "English (US)",
     value: "en_US",
   },
-  {
-    label: "English (UK)",
-    value: "en_GB",
-  },
-  {
-    label: "Čeština (Czech)",
-    value: "cs_CZ",
-  },
+  // {
+  //   label: "English (UK)",
+  //   value: "en_GB",
+  // },
+  // {
+  //   label: "Čeština (Czech)",
+  //   value: "cs_CZ",
+  // },
   {
     label: "简体中文 (Chinese, Simplified)",
     value: "zh_CN",
   },
-  {
-    label: "繁體中文 (Chinese, Traditional)",
-    value: "zh_TW",
-  },
-  {
-    label: "Deutsch (German)",
-    value: "de_DE",
-  },
-  {
-    label: "Español (Spanish)",
-    value: "es_ES",
-  },
-  {
-    label: "Français (French)",
-    value: "fr_FR",
-  },
-  {
-    label: "Italiano (Italian)",
-    value: "it_IT",
-  },
-  {
-    label: "日本語 (Japanese)",
-    value: "ja_JP",
-  },
-  {
-    label: "한국어 (Korean)",
-    value: "ko_KR",
-  },
-  {
-    label: "Magyar (Hungarian)",
-    value: "hu_HU",
-  },
-  {
-    label: "Nederland (Dutch, Netherlands)",
-    value: "nl_NL",
-  },
-  {
-    label: "Norsk Bokmål (Norwegian)",
-    value: "nb_NO",
-  },
-  {
-    label: "Português (Portuguese, Brazil)",
-    value: "pt_BR",
-  },
-  {
-    label: "Português (Portuguese, Portugal)",
-    value: "pt_PT",
-  },
-  {
-    label: "Polskie (Polish)",
-    value: "pl_PL",
-  },
-  {
-    label: "فارسی (Persian)",
-    value: "fa_IR",
-  },
-  {
-    label: "Svenska (Swedish)",
-    value: "sv_SE",
-  },
-  {
-    label: "Türkçe (Turkish)",
-    value: "tr_TR",
-  },
-  {
-    label: "Українська (Ukrainian)",
-    value: "uk_UA",
-  },
-  {
-    label: "Tiếng Việt (Vietnamese)",
-    value: "vi_VN",
-  },
+  // {
+  //   label: "繁體中文 (Chinese, Traditional)",
+  //   value: "zh_TW",
+  // },
+  // {
+  //   label: "Deutsch (German)",
+  //   value: "de_DE",
+  // },
+  // {
+  //   label: "Español (Spanish)",
+  //   value: "es_ES",
+  // },
+  // {
+  //   label: "Français (French)",
+  //   value: "fr_FR",
+  // },
+  // {
+  //   label: "Italiano (Italian)",
+  //   value: "it_IT",
+  // },
+  // {
+  //   label: "日本語 (Japanese)",
+  //   value: "ja_JP",
+  // },
+  // {
+  //   label: "한국어 (Korean)",
+  //   value: "ko_KR",
+  // },
+  // {
+  //   label: "Magyar (Hungarian)",
+  //   value: "hu_HU",
+  // },
+  // {
+  //   label: "Nederland (Dutch, Netherlands)",
+  //   value: "nl_NL",
+  // },
+  // {
+  //   label: "Norsk Bokmål (Norwegian)",
+  //   value: "nb_NO",
+  // },
+  // {
+  //   label: "Português (Portuguese, Brazil)",
+  //   value: "pt_BR",
+  // },
+  // {
+  //   label: "Português (Portuguese, Portugal)",
+  //   value: "pt_PT",
+  // },
+  // {
+  //   label: "Polskie (Polish)",
+  //   value: "pl_PL",
+  // },
+  // {
+  //   label: "فارسی (Persian)",
+  //   value: "fa_IR",
+  // },
+  // {
+  //   label: "Svenska (Swedish)",
+  //   value: "sv_SE",
+  // },
+  // {
+  //   label: "Türkçe (Turkish)",
+  //   value: "tr_TR",
+  // },
+  // {
+  //   label: "Українська (Ukrainian)",
+  //   value: "uk_UA",
+  // },
+  // {
+  //   label: "Tiếng Việt (Vietnamese)",
+  //   value: "vi_VN",
+  // },
 ];
 
 export const languages = languageOptions.map((i) => i.value);
